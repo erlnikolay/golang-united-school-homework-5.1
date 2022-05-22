d3bd6d1ce69fd4756e9beed6f03c0b22776492a8
erlnikolay <erl.nikolay@gmail.com>
2022-05-22 16:53:11 +0700

    Solution 1

--- square.go
+++ square.go
@@ -1,5 +1,11 @@
 package square
 
+//package main
+
+import (
+	"math"
+)
+
 type Point struct {
 	x, y int
 }
@@ -9,14 +15,31 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
+func (s Square) End() Point {
 	// implement me
+	return Point{s.start.x + int(s.a), s.start.y + int(s.a)}
 }
 
-func (receiver) Area() uint {
+func (s Square) Area() uint {
 	// implement me
+	return uint(math.Pow(float64(s.a), 2))
 }
 
-func (receiver) Perimeter() uint {
+func (s Square) Perimeter() uint {
 	// implement me
+	return uint(4 * s.a)
 }
+
+//func main() {
+//	var resPoint Point
+//	var areaSquare, perimeterSquare uint
+//
+//	var sInput Square = Square{Point{1, 1}, 1}
+//	resPoint = sInput.End()
+//	areaSquare = sInput.Area()
+//	perimeterSquare = sInput.Perimeter()
+//	fmt.Printf("Begin point, X: %d, Y: %d, lenght of side: %d\n", sInput.start.x, sInput.start.y, sInput.a)
+//	fmt.Printf("End point, X: %d, Y: %d\n", resPoint.x, resPoint.y)
+//	fmt.Printf("Area square: %d\n", areaSquare)
+//	fmt.Printf("Perimeter square: %d\n", perimeterSquare)
+//}
